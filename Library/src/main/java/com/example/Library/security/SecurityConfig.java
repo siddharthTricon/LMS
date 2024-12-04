465b096695a1e5446aa3b5fa62fcbb09ca13f1f9
Siddharth Goswami <siddharth.goswami@Triconinfotech.com>
2024-12-04 16:55:17 +0530

    Added Library System

--- Library/src/main/java/com/example/Library/security/SecurityConfig.java
+++ Library/src/main/java/com/example/Library/security/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
+    public BCryptPasswordEncoder     passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
